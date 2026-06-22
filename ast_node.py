5d3b40da8f4968d9b56c4386fac6e57a014028aa
Ananyo Bhattacharya <169988112+UniquePython@users.noreply.github.com>
2026-06-22 22:41:23 +0530

    Remove unnecessary repr from ast nodes since dataclasses cover it

--- ast_node.py
+++ ast_node.py
@@ -10,18 +10,14 @@ class BinaryOp(Enum):
     DIV = auto()
 
 
+Expr = Union["NumberLiteral", "BinaryExpr"]
+
+
 @dataclass(frozen=True)
 class NumberLiteral:
     value: int
-
     start: int
     end: int
-
-    def __repr__(self) -> str:
-        return f"NumberLiteral => {self.value}"
-
-
-Expr = Union["NumberLiteral", "BinaryExpr"]
 
 
 @dataclass(frozen=True)
@@ -29,23 +25,5 @@ class BinaryExpr:
     operator: BinaryOp
     left: Expr
     right: Expr
-
     start: int
     end: int
-
-    def __repr__(self) -> str:
-        return f"BinaryExpr => [{self.left}]->[{self.operator}]<-[{self.right}]"
-
-
-if __name__ == "__main__":
-    from tokens import TokenKind
-
-    print(
-        BinaryExpr(
-            TokenKind.PLUS,
-            NumberLiteral(1, 1, 2),
-            BinaryExpr(TokenKind.ASTERISK, NumberLiteral(2), NumberLiteral(3), 1, 2),
-            1,
-            2,
-        )
-    )
